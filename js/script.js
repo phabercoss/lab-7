f4e037a42b0b156695cbf7ba676ce39b1230521a
Phallyn <phabercoss@hawk.iit.edu>
2019-04-16 14:13:40 -0500

    Fixed possible java script errors

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
+var message = document.getElementById('entryinput').value;
 var button = document.getElementById('entrybutton');
-button.addEventListener('click', function () {
-var message = document.getElementbyId('entryinput').value;
 
+button.addEventListener('click', function () {
 alert("Phallyn Habercoss: " + message);
 console.log(message);
-document.getElementbyId('textoutput').innerHTML = message;
+document.getElementById('textoutput').innerHTML = message;
 });
-
-
 }
 window.addEventListener('load', init);
