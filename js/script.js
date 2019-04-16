f54af04f661290d879955c6edf0b2025256825ff
Phallyn <phabercoss@hawk.iit.edu>
2019-04-16 15:43:51 -0500

    Fixed final java script issue

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
-var message = document.getElementById('entryinput').value;
-var button = document.getElementById('entrybutton');
-
+var button = document.getElementById("entrybutton");
 button.addEventListener('click', function () {
+var message = document.getElementById("entryinput").value;
+
 alert("Phallyn Habercoss: " + message);
 console.log(message);
-document.getElementById('textoutput').innerHTML = message;
+document.getElementById("textoutput").innerHTML = message;
 });
 }
 window.addEventListener('load', init);
